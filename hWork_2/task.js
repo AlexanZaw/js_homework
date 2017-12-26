00a17cabfaf884431c6d341bfbb578bd45446b57
AlexanZaw <aberfurerr@gmail.com>
2017-12-26 08:57:00 +0200

    HomeWork

--- hWork_2/task.js
+++ hWork_2/task.js
@@ -1,22 +1,24 @@
 let total = 0; //переменная для подсчёта общего результата
 
-for (i = 0; i < 15; i++){
+for (i = 1; i <= 15; i++){
     var first = Math.floor((Math.random() * 6) + 1);
     var second = Math.floor((Math.random() * 6) + 1);
-
-    if (i == 8 || i ==13 ){
+    
+    document.getElementById("result").innerHTML +="<b>" + "Бросок № " + i + ". " + "</b><p>";    
+    if (i == 8 || i == 13 ){
         continue;
     }
-
-    document.getElementById("result").innerHTML += "Первая кость: " + first + "Вторая кость: " + second + " ";
+    document.getElementById("result").innerHTML += "Первая кость: " + first + " Вторая кость: " + second + " " + "<p>";
     
     if (first == second){
-        document.getElementById("result").innerHTML += "Выпал дубль. Число " + first;
+        document.getElementById("result").innerHTML += "Выпал дубль. Число " + first + "<p>";
     }
 
     if (first < 3 && second > 4){
-        document.getElementById("result").innerHTML += "Большой разброс между костями. Разница составляет " + (second - first);
+        document.getElementById("result").innerHTML += "Большой разброс между костями. Разница составляет " + (second - first) + "<p>";
     }
-    total += (first + second);
+    total += first + second;
+    document.getElementById("result").innerHTML += "<p>";
 }
-total > 100 ? document.getElementById("result").innerHTML +="Победа!!! Вы набрали: " + total : document.getElementById("result").innerHTML += "Вы проиграли. У Вас " + total + " очков";
+total > 100 ? document.getElementById("result").innerHTML +="Победа!!! Вы набрали: " + total  + "<p>" : document.getElementById("result").innerHTML += "Вы проиграли. У Вас " + total + " очков" + "<p>";
+
--- hWork_2/task_3.js
+++ hWork_2/task_3.js
@@ -1,5 +1,5 @@
-for (var i = 0; i <= 100; i += 10){
-   if (100 - i == 30 || 100 - i == 70 || 100 - i == 90){
+for (var i = 100; i >= 0; i -= 10){
+   if (i == 30 || i == 70 || i == 90){
        continue;
    }
      elemResult.innerHTML += 'Число: <b>' + 100 - i + '</b><br>';
